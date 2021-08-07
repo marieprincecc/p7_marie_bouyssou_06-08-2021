15ed8168963d71b497886377a739e04e14a3f909
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-08-07 20:26:10 +0200

    ajout vue poste/:id

--- groupomania/src/views/poste.vue
+++ groupomania/src/views/poste.vue
@@ -1,13 +1,16 @@
 <template>
   <navbarVue/>
+   <publicationVue/>
 </template>
 
 <script>
 import navbarVue from '../components/navbar.vue'
+import publicationVue from '../components/publication.vue'
 export default {
 name: 'Publication',
 components:{
      navbarVue,
+     publicationVue,
      }
 }
 </script>
