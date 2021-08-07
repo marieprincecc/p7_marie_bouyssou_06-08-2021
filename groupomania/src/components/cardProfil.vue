29a5f67d9c1586b071dbba2405ad0ba3efd5542a
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-08-07 20:27:11 +0200

    'ajout vue poste/:id'

---
+++ groupomania/src/components/cardProfil.vue
@@ -0,0 +1,36 @@
+<template>
+  <div class="container-fluid p-5 bg" >
+      <div class="row">
+          <div class="col md-p-5">
+              <h1>Nom prenom</h1>
+          </div>
+          
+      </div>
+
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+ body, html {
+    height: 100%;
+  }
+  .bg {
+    /* The image used */
+    background-image: url("../../public/icon.svg");
+
+    /* Full height */
+    height: 100%;
+
+    /* Center and scale the image nicely */
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: 30%;
+  }
+</style>
+
---
+++ groupomania/src/components/publication.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="card" >
+        <div class="card-header">
+    qui?
+        </div>
+  <img src="" class="card-img-top" alt="...">
+  <div class="card-body">
+       <h5 class="card-title">Card title</h5>
+    <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+    <button type="button" class="btn btn-warning text-white me-3">
+  J'aime! <span class="badge bg-secondary">4</span>
+</button>
+<button type="button" class="btn btn-warning text-white"><router-link to="/poste/:id">
+  Commentaires</router-link><span class="badge bg-secondary">4</span>
+</button>
+
+  </div>
+</div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
