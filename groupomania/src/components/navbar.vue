5af8416251d75f7e1f78e77d37f8ed9d70af0c28
Marie Bouyssou <reveolution.de.saveur@gmail.com>
2021-08-07 16:43:52 +0200

    remise a jour

--- groupomania/src/components/navbar.vue
+++ groupomania/src/components/navbar.vue
@@ -1,37 +1,28 @@
 <template>
-<div id="navbar">
-  <router-link to="/acceuil"><img src="../assets/icon-left-font.png" alt="logo"></router-link> 
- <router-link to="/accueil"></router-link>
- <router-link to="/profil"></router-link>
- <router-link to="/poste"></router-link>
-  </div>
-  
+<nav class="col navbar navbar-expand-md navbar-warning">
+  <router-link class="navbar-brand text-left text-sm-left" to="/accueil"><img src="../assets/icon-left-font-monochrome-white.svg" alt="logo groupomannia"/></router-link>
+                        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarContent">
+                            <span class="navbar-toggler-icon"></span>
+                        </button>
+                        <div id="navbarContent" class="collapse navbar-collapse ">
+                            <ul class="navbar-nav ">
+                                <li class="nav-item">
+                                    <router-link class="nav-link" to="/accueil">Acceuil</router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link class="nav-link" to="/profil/:id">Profil</router-link>
+                                </li>
+                                <li class="nav-item">
+                                    <router-link class="nav-link" to="/poste">Nouvelle publication</router-link>
+                                </li>
+                            </ul>
+                        </div>
+                    </nav>
 </template>
 
 <script>
-export default {
-
-}
+export default {};
 </script>
 
-<style scoped lang="css">
-#navbar {
-  display: flex;
-  align-items: flex-start;
-  justify-content: space-between;
-  
-}
-
-
-ul {
-    list-style: none;
-    color: #fd2d01;
-    display: flex;
-    align-items: flex-end;
-    
-    }
-li {
-    margin: 10px;
-}
-
+<style>
 </style>
